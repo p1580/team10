c1247ab94c51d0cf81c349c7d6ddf204a2657cbd
DESKTOP-UITHU5S\F309 <kasx156@gmail.com>
2021-12-15 11:08:01 +0800

    edit reservation

--- database/migrations/2021_12_15_030554_create_reservation_table.php
+++ database/migrations/2021_12_15_030554_create_reservation_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBorrowclassTable extends Migration
+class CreateReservationTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateBorrowclassTable extends Migration
      */
     public function up()
     {
-        Schema::create('borrowclass', function (Blueprint $table) {
+        Schema::create('reservation', function (Blueprint $table) {
             $table->id();
             $table->string('date',191)->comment('日期');
-            $table->Integer('card_id')->comment('班級');
-            $table->string('cid')->comment('教室');
+            $table->Integer('card_id')->comment('學生證卡號');
+            $table->string('cid')->comment('教室編號');
             $table->string('start_at')->comment('借用時間');
             $table->string('end_of')->comment('結束時間');
             $table->date('memo')->comment('備註');
@@ -32,6 +32,6 @@ class CreateBorrowclassTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('borrowclass');
+        Schema::dropIfExists('reservation');
     }
 }
